19ba7860c7cb187ce596c807182b240f3d7afefb
saiganesh-sristla <saiganesh3108@gmail.com>
2025-06-30 13:25:01 +0530

    added task update and delete endpoint

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -12,4 +12,19 @@ router.get('/', async (req, res) => {
   res.json(tasks);
 });
 
+router.put('/:id', async (req, res) => {
+  const task = await Task.findById(req.params.id);
+  const data = req.body;
+
+  await task.updateOne(data);
+  res.status(200).json(task);
+})
+
+router.put('/:id', async (req, res) => {
+  const task = await Task.findById(req.params.id);
+
+  await task.deleteOne();
+  res.status(200).json(task);
+})
+
 module.exports = router;
